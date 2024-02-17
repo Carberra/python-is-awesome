1b0988ddc9a105dc5329394b8a7cfd12e7f1369d
Ethan Henderson <ethan.henderson.1998@gmail.com>
2024-02-17 11:15:40 +0000

    Remove Python but Faster episodes

--- optimisations/containment-checks.py
+++
@@ -1,5 +0,0 @@
-numbers = {1, 2, 76, 4, 90, 14}
-
-x = int(input("Enter a number: "))
-if x in numbers:
-    ...
--- optimisations/dict-key.py
+++
@@ -1,4 +0,0 @@
-x = {}
-for i in range(255):
-    # x.update({chr(i): i})
-    x[chr(i)] = i
